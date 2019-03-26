3c5347594de411e98b989b37fc03c37530bc1937
Matt O'Tousa <matt@mattotousa.com>
2019-03-26 12:55:09 -0700

    loads songs from directory

--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,14 @@ extern crate sciter;
 
 
 use std::fs::File;
-use std::{thread, time};
-use sciter::{Element, Value};
+// use std::{thread, time};
+use sciter::Element;
 use self::sciter::dom::event::*;
 use self::sciter::dom::HELEMENT;
 use std::io::BufReader;
 use rodio::Sink;
-//
-struct PlayButton;
+use walkdir::WalkDir;
+use sublime_fuzzy::best_match;
 
 struct Song {
     path: String,
@@ -22,8 +22,8 @@ struct Song {
 struct Player {
     device: rodio::Device,
     sink: Sink,
-    current_song: Option<Song>,
-    library: Option<Box<Song>>
+    current_song_index: i64,
+    library: Vec<Song>
 }
 
 impl Player {
@@ -33,14 +33,14 @@ impl Player {
         Player {
             sink,
             device,
-            current_song: None,
-            library: None
+            current_song_index: -1,
+            library: Vec::new()
         }
     }
 
     // Sink::stop() seems to stop the sink forever for some reason.
     // So to stop a playing sound we have to destroy the sink and
-    // make a new one.
+    // make a new one. (?)
     fn reset_sink(&mut self) {
         if !self.sink.empty() {
             self.sink = Sink::new(&self.device);
@@ -48,11 +48,12 @@ impl Player {
     }
 
     // TODO return proper error
-    pub fn load_song(&mut self, song: &Song) -> bool {
+    pub fn load_song(&mut self, song: Song) -> bool {
         match File::open(&song.path) {
             Ok(file) =>
                 match rodio::Decoder::new(BufReader::new(file)) {
                     Ok(source) => {
+                        // self.current_song = Some(song);
                         self.reset_sink();
                         self.sink.append(source);
                         return true;
@@ -61,13 +62,6 @@ impl Player {
                 },
             Err(_) => return false,
         }
-        /*
-        let file = File::open(&song.path).unwrap();
-        let source = rodio::Decoder::new(BufReader::new(file)).unwrap();
-        self.reset_sink();
-        self.sink.append(source);
-        true
-        */
     }
 
     pub fn pause(&self) {
@@ -77,22 +71,27 @@ impl Player {
     pub fn play(&self) {
         self.sink.play();
     }
+
+    pub fn load_folder(&mut self, dir : &str) {
+        for entry in WalkDir::new(dir).follow_links(true).into_iter().filter_map(|e| e.ok()) {
+            // println!("{}", entry.path().display());
+            let path = entry.path().to_str();
+            match path {
+                Some(p) => {
+                    let song = Song { path: p.to_string() };
+                    self.library.push(song);
+                },
+                None => continue,
+            }
+        }
+    }
 }
 
 
 fn main() {
     let mut player = Player::new();
-    // let song = Song { path: "test.mp3".to_string() };
-    // player.load_song(&song);
-    // player.play();
-    // thread::sleep(time::Duration::from_millis(3000));
-    // player.pause();
-    // thread::sleep(time::Duration::from_millis(1000));
-    // player.load_song(&song);
-    // player.play();
-    // thread::sleep(time::Duration::from_millis(3000));
+    player.load_folder("test_songs");
     let html = include_bytes!("minimal.html");
-    // let mut frame = sciter::Window::new();
     let mut frame = sciter::Window::new();
     frame.event_handler(player);
     frame.load_html(html, Some("example://minimal.html"));
@@ -100,7 +99,7 @@ fn main() {
     println!("hi");
 }
 
-impl sciter::EventHandler for Player {
+impl sciter::EventHandler for Player{
     fn on_event(&mut self, root: HELEMENT, source: HELEMENT,
                 target: HELEMENT, code: BEHAVIOR_EVENTS,
                 phase: PHASE_MASK, reason: EventReason) -> bool {
@@ -119,11 +118,10 @@ impl sciter::EventHandler for Player {
             let song_path = root.find_first("#user-input").unwrap()
                           .expect("div#user-input not found!");
 
-
             if let Some(id) = source.get_attribute("id") {
                 match id.as_str() {
                     "load-button" => {
-                        self.load_song(&Song { path: song_path.get_text() });
+                        self.load_song(Song { path: song_path.get_text() });
                         return true;
                     },
                     "play-button" => {
@@ -136,46 +134,13 @@ impl sciter::EventHandler for Player {
                     }
                     _ => panic!("ID '{}' not recognized!", id),
                 }
-            };
-        };
+            }
+        }
+
+        if code == BEHAVIOR_EVENTS::CONTENT_CHANGED {
+
+        }
 
         false
     }
 }
-
-/*
-impl sciter::EventHandler for PlayButton {
-    fn on_event(&mut self, root: HELEMENT, source: HELEMENT,
-                target: HELEMENT, code: BEHAVIOR_EVENTS,
-                phase: PHASE_MASK, reason: EventReason) -> bool {
-
-        if code == BEHAVIOR_EVENTS::BUTTON_CLICK {
-            let root = Element::from(root).root();
-            let message = root.find_first("#song-title").unwrap()
-                          .expect("div#song-title not found!");
-            let source = Element::from(source);
-
-            println!("root: {:?}, message: {:?}, source: {:?}",
-                     root, message, source);
-
-            if let Some(id) = source.get_attribute("id") {
-                if id == "send" {
-                    source.send_event(BEHAVIOR_EVENTS::CHANGE, None,
-                                      Some(message.as_ptr()))
-                                      .expect("Failed to send event");
-                    return true;
-                } else if id == "fire" {
-                    let data = Value::from("Rusty param");
-                    source.fire_event(BEHAVIOR_EVENTS::CHANGE, None,
-                                      Some(message.as_ptr()), false,
-                                      Some(data)).expect("Failed to 
-                                      fire event");
-                    return true;
-                }
-            };
-        };
-
-        false
-    }
-}
-*/
