a118f53c27539cfcc4a139cb4483dd272e0f32ff
Matt O'Tousa <matt@mattotousa.com>
2019-04-02 19:11:52 -0700

    Global Hotkey added
    Right now it's 'M' which is terrible, and the music player doesn't keep
    playing music if the UI is closed because it's totally not set up right
    yet, but it does exist!

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,6 @@ extern crate sciter;
 
 
 use std::fs::File;
-// use std::{thread, time};
 use sciter::Element;
 use self::sciter::dom::event::*;
 use self::sciter::dom::HELEMENT;
@@ -14,6 +13,8 @@ use std::io::BufReader;
 use rodio::Sink;
 use walkdir::WalkDir;
 use sublime_fuzzy::{best_match, format_simple};
+// use inputbot::{*};
+use inputbot;
 
 struct Song {
     path: String,
@@ -24,8 +25,21 @@ struct Player {
     sink: Sink,
     current_song_index: usize,
     library: Vec<Song>,
-    ui_ready: bool,
+    is_ui_open: bool,
+    is_ui_ready: bool,
     top_match_index: usize
+}
+
+struct UIMatch {
+    match_type: UIMatchType,
+    score: isize,
+    text: String,
+    index: usize
+}
+
+enum UIMatchType {
+    SONG,
+    OPERATION
 }
 
 impl Player {
@@ -37,16 +51,38 @@ impl Player {
             device,
             current_song_index: 0,
             library: Vec::new(),
-            ui_ready: false,
+            is_ui_open: false,
+            is_ui_ready: false,
             top_match_index: 0
         }
     }
 
+    pub fn get_matches(&self, query: &str) -> Vec<UIMatch> {
+        let mut matches = Vec::new();
+        let open_tag = "<span style=\"color:red\">";
+        let close_tag = "</span>";
+        let mut i = 0;
+        for song in &self.library {
+            let result = best_match(query, song.path.as_str());
+            match result {
+                Some(result) => matches.push(UIMatch {
+                        match_type: UIMatchType::SONG,
+                        score: result.score(),
+                        index: i,
+                        text: format!("[TYPE: SONG, SCORE: {}, INDEX: {}] {}",
+                                      result.score(),
+                                      i,
+                                      format_simple(&result, &song.path, open_tag, close_tag)).to_string(),
+                    }),
+                None => ()
+            }
+            i += 1;
+        }
+        matches
+    }
+
     pub fn load_top_match(&mut self) {
-        // Ok song probably needs a copy constructor?
-        self.load_song(&Song { path: self.library[self.top_match_index].path.to_string() });
-        // Can i make something like this work??
-        // self.load_song(self.library[self.top_match_index]);
+        self.load_song_from_library(self.top_match_index);
     }
 
     // Sink::stop() seems to stop the sink forever for some reason.
@@ -60,18 +96,29 @@ impl Player {
 
     // TODO return proper error
     pub fn load_song(&mut self, song: &Song) -> bool {
-        match File::open(&song.path) {
+        return self.load_song_from_path(&song.path);
+    }
+
+    pub fn load_song_from_library(&mut self, index: usize) -> bool {
+        // let pathstring = &self.library[index].path;
+        // return self.load_song_from_path(pathstring.as_str());
+        let pathstring = &self.library[index].path.clone();
+        return self.load_song_from_path(pathstring.as_str());
+    }
+
+    pub fn load_song_from_path(&mut self, song_path: &str) -> bool {
+        return match File::open(&song_path) {
             Ok(file) =>
                 match rodio::Decoder::new(BufReader::new(file)) {
                     Ok(source) => {
                         // self.current_song = Some(song);
                         self.reset_sink();
                         self.sink.append(source);
-                        return true;
+                        true
                     },
-                    Err(_) => return false,
+                    Err(_) => false,
                 },
-            Err(_) => return false,
+            Err(_) => false,
         }
     }
 
@@ -85,12 +132,15 @@ impl Player {
 
     pub fn load_folder(&mut self, dir : &str) {
         for entry in WalkDir::new(dir).follow_links(true).into_iter().filter_map(|e| e.ok()) {
-            // println!("{}", entry.path().display());
             let path = entry.path().to_str();
             match path {
                 Some(p) => {
-                    let song = Song { path: p.to_string() };
-                    self.library.push(song);
+                    if p.ends_with(".mp3") {
+                        // let song = Song { path: p.to_string() };
+                        self.library.push(Song {
+                            path: p.to_string()
+                        });
+                    }
                 },
                 None => continue,
             }
@@ -100,13 +150,17 @@ impl Player {
 
 
 fn main() {
-    let mut player = Player::new();
-    player.load_folder("test_songs");
-    let html = include_bytes!("minimal.html");
-    let mut frame = sciter::Window::new();
-    frame.event_handler(player);
-    frame.load_html(html, Some("example://minimal.html"));
-    frame.run_app();
+    inputbot::KeybdKey::MKey.bind(|| {
+        let mut player = Player::new();
+        player.load_folder("test_songs");
+        let html = include_bytes!("minimal.html");
+        println!("yooooooo");
+        let mut frame = sciter::Window::new();
+        frame.event_handler(player);
+        frame.load_html(html, Some("example://minimal.html"));
+        frame.run_app();
+    });
+    inputbot::handle_input_events();
     println!("hi");
 }
 
@@ -116,10 +170,10 @@ impl sciter::EventHandler for Player{
                 target: HELEMENT, code: BEHAVIOR_EVENTS,
                 phase: PHASE_MASK, reason: EventReason) -> bool {
         if phase != PHASE_MASK::SINKING { return false; }
+
         if code == BEHAVIOR_EVENTS::DOCUMENT_READY {
-            self.ui_ready = true;
-        }
-        if !self.ui_ready {
+            self.is_ui_ready = true;
+        } else if !self.is_ui_ready {
             return false;
         }
 
@@ -128,65 +182,86 @@ impl sciter::EventHandler for Player{
         let mut last_event_element = root.find_first("#last-event").unwrap()
                         .expect("div#last-event not found!");
         let id = source.get_attribute("id").unwrap_or_else(|| "_".to_string());
-        if id != "last-event" {
+
+        if id != "last-event" && id != "debug" {
             let result = last_event_element.set_text(format!("\nEVENT: {:?} ({})", code, source).as_str());
-        }
-        match code {
-            BEHAVIOR_EVENTS::BUTTON_CLICK => return on_click(self, root, source),
-            BEHAVIOR_EVENTS::EDIT_VALUE_CHANGED => return on_input(self, root, source),
-            _ => (),
+            println!("\nEVENT: {:?} ({})", code, source);
         }
 
-        false
+        match code {
+            BEHAVIOR_EVENTS::DOCUMENT_READY => return update_matches(self, root, source),
+            BEHAVIOR_EVENTS::BUTTON_CLICK => return on_click(self, root, source),
+            BEHAVIOR_EVENTS::EDIT_VALUE_CHANGED => return on_input(self, root, source),
+            _ => false
+        }
     }
+}
+
+fn update_matches(player: &mut Player, root: Element, source: Element) -> bool {
+    let mut matches_el = root.find_first("#matches").unwrap()
+        .expect("div#matches element not found");
+    let user_input = root.find_first("#user-input").unwrap()
+        .expect("div#user-input not found!");
+    // for some reason this will take a br raw string
+    // but not " " (?) TODO find out why.
+    let result = matches_el.set_html(br#" "#, None).unwrap();
+    let mut ui_matches = player.get_matches(user_input.get_text().as_str());
+    ui_matches.sort_by(|b,a| a.score.cmp(&b.score));
+    let mut top_score = 0;
+    let mut top_index = 0;
+    for m in ui_matches {
+        let mut el = Element::create("li").unwrap();
+        let result = matches_el.append(&el);
+        let result = el.set_html(m.text.as_str().as_bytes(), None);
+        if m.score >= top_score {
+            top_score = m.score;
+            top_index = m.index;
+        }
+    }
+    player.top_match_index = top_index;
+    true
+    /*
+    let mut i = 0;
+    let mut top_score = 0;
+    let mut top_index = 0;
+    for song in &player.library {
+        let search_box = best_match(user_input.get_text().as_str(), song.path.as_str());
+        match search_box {
+            Some(search) => {
+            if search.score() >= top_score {
+                top_score = search.score();
+                top_index = i;
+            }
+            let mut formatted_match = String::from(format!(" [score: {}]", search.score()).as_str());
+            formatted_match.push_str(format!(" [index: {}]", i).as_str());
+            formatted_match.push_str(format_simple(&search, &song.path, "<span style=\"color:red\">", "</span>").as_str());
+            let mut el = Element::create("li").unwrap();
+            let result = matches_el.append(&el);
+            let result = el.set_html(formatted_match.as_str().as_bytes(), None);
+            }
+            None => {
+                let mut el = Element::create("li").unwrap();
+                let result = matches_el.append(&el);
+                let result = el.set_html(format!("<span style=\"color: lightgrey\">{}</span>", song.path).as_bytes(), None);
+            },
+        }
+        i += 1;
+    }
+    player.top_match_index = top_index;
+    println!("Top Match Score: {}", top_score);
+    println!("Top Match Index: {}", player.top_match_index);
+    return true;
+    */
 }
 
 fn on_input(player: &mut Player, root: Element, source: Element) -> bool {
     if let Some(id) = source.get_attribute("id") {
-        match id.as_str() {
-            "user-input" => {
-                let mut matches_el = root.find_first("#matches").unwrap()
-                    .expect("div#matches element not found");
-                let user_input = root.find_first("#user-input").unwrap()
-                    .expect("div#user-input not found!");
-                // for some reason this will take a br raw string
-                // but not " " (?) TODO find out why.
-                let result = matches_el.set_html(br#" "#, None).unwrap();
-                let mut i = 0;
-                let mut top_score = 0;
-                let mut top_index = 0;
-                for song in &player.library {
-                    let search_box = best_match(user_input.get_text().as_str(), song.path.as_str());
-                    match search_box {
-                        Some(search) => {
-                        if search.score() >= top_score {
-                            top_score = search.score();
-                            top_index = i;
-                        }
-                        let formatted_match = format_simple(&search, &song.path, "<span style=\"color:red\">", "</span>");
-                        let mut el = Element::create("li").unwrap();
-                        let result = matches_el.append(&el);
-                        let result = el.set_html(formatted_match.as_str().as_bytes(), None);
-                        let result = el.append(&Element::with_text("span", format!(" [score: {}]", search.score()).as_str()).unwrap());
-                        let result = el.append(&Element::with_text("span", format!(" [index: {}]", i).as_str()).unwrap());
-                        let result = matches_el.append(&Element::create("br").unwrap());
-                        }
-                        None => {
-                            let mut el = Element::create("li").unwrap();
-                            let result = el.set_text(song.path.as_str());
-                        },
-                    }
-                    i += 1;
-                }
-                player.top_match_index = top_index;
-                println!("Top Match Score: {}", top_score);
-                println!("Top Match Index: {}", player.top_match_index);
-                return true;
-            }
-            _ => return false
-        }
+        return match id.as_str() {
+            "user-input" => update_matches(player, root, source),
+            _ => false
+        };
     }
-    false
+    return false;
 }
 
 fn on_click(player: &mut Player, root: Element, source: Element) -> bool {
